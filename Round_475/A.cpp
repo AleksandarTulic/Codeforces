833ac164a01246c27a3c41c8a54ea8ac68d28adb
Aleksandar <39736298+Tula99@users.noreply.github.com>
2019-07-24 12:51:19 +0200

    Create A.cpp

---
+++ Round_475/A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <set>
+#include <map>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <cmath>
+#include <iomanip>
+#include <algorithm>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin>>n;
+ 
+    cout<<n/2+1<<endl;
+    return 0;
+}
