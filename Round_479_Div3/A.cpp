23cded03e5da11c3280c92afcc99fad821af61ef
Aleksandar <39736298+Tula99@users.noreply.github.com>
2019-07-25 09:13:23 +0200

    Create A.cpp

---
+++ Round_479_Div3/A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
+#include <cmath>
+#include <map>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+ 
+    for (int i=0;i<k;i++)
+        if ( n % 10 != 0 ) n--;
+        else n/=10;
+ 
+    cout<<n<<endl;
+    return 0;
+}
